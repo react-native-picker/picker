9064706bd9c46eb9331412e8dc18fbc3d4096ded
jainkuniya <jainkuniya@gmail.com>
2019-03-17 14:04:21 +0530

    Add 'babel-plugin-module-resolver'.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@react-native-community/picker": "./js"
+        },
+        cwd: "babelrc"
+      }
+    ]
+  ]
 };
