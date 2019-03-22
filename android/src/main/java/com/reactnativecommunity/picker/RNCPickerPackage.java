d9ebb983d48fce737c0860fcb4e446dfffbff8ef
Vishwesh Jainkuniya <jainkuniya@gmail.com>
2019-03-22 22:04:30 +0530

    android: Register DialogPicker & DropdownPicker as view manager.

--- android/src/main/java/com/reactnativecommunity/picker/RNCPickerPackage.java
+++ android/src/main/java/com/reactnativecommunity/picker/RNCPickerPackage.java
@@ -1,6 +1,7 @@
 
 package com.reactnativecommunity.picker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -13,7 +14,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RNCPickerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNCPickerModule(reactContext));
+      return Collections.emptyList();
     }
 
     // Deprecated from RN 0.47
@@ -23,6 +24,9 @@ public class RNCPickerPackage implements ReactPackage {
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        List<ViewManager> list = new ArrayList<>();
+        list.add(new ReactDialogPickerManager());
+        list.add(new ReactDropdownPickerManager());
+      return list;
     }
 }
