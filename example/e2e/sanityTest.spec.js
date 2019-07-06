25e454340e68b48214f8e484663a4e47efa514bf
jainkuniya <jainkuniya@gmail.com>
2019-07-06 12:29:11 +0530

    Add detox helpers

---
+++ example/e2e/sanityTest.spec.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+const {device, expect, element, by} = require('detox');
+
+describe('Picker', () => {
+  beforeEach(async () => {
+    await device.launchApp({
+      permissions: {},
+    });
+  });
+
+  it('should load example app with no errors and show all the examples by default', async () => {
+    await expect(element(by.text('Picker Examples'))).toExist();
+  });
+});
