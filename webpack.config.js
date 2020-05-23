00fa0d8d69323689ccf5100efcd7b99c98d05cff
Evan Bacon <baconbrix@gmail.com>
2020-05-22 21:01:39 -0700

    Delete extra example modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
-const node_modules = path.resolve(__dirname, '..', 'node_modules');
+// const node_modules = path.resolve(__dirname, '..', 'node_modules');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
@@ -15,12 +15,12 @@ module.exports = async function (env, argv) {
     use: require.resolve('babel-loader'),
   });
 
-  Object.assign(config.resolve.alias, {
-    react: path.resolve(node_modules, 'react'),
-    'react-native': path.resolve(node_modules, 'react-native-web'),
-    'react-native-web': path.resolve(node_modules, 'react-native-web'),
-    '@react-native-community/picker': path.resolve('../js'),
-  });
+  // Object.assign(config.resolve.alias, {
+  //   react: path.resolve(node_modules, 'react'),
+  //   'react-native': path.resolve(node_modules, 'react-native-web'),
+  //   'react-native-web': path.resolve(node_modules, 'react-native-web'),
+  //   '@react-native-community/picker': path.resolve('../js'),
+  // });
 
   return config;
 };
