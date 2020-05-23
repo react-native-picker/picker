ddaeedbb870f77ecb8a1ee58a24eb72f620b8b21
Evan Bacon <baconbrix@gmail.com>
2020-05-22 20:08:21 -0700

    Update webpack.config.js

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
-const root = path.resolve(__dirname, '..');
-const node_modules = path.resolve(__dirname, 'node_modules');
+const node_modules = path.resolve(__dirname, '..', 'node_modules');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
+  config.context = path.resolve(__dirname, '..');
+
   config.module.rules.push({
     test: /\.(js|ts|tsx)$/,
-    include: path.resolve('../js'),
-    use: 'babel-loader',
+    include: /(js|example)\/.+/,
+    exclude: /node_modules/,
+    use: require.resolve('babel-loader'),
   });
 
   Object.assign(config.resolve.alias, {
-    react: path.join(node_modules, 'react'),
-    'react-native-web': path.join(node_modules, 'react-native-web'),
-    '@react-native-community/picker': path.join(
-      root,
-      require('../package.json').main,
-    ),
+    react: path.resolve(node_modules, 'react'),
+    'react-native': path.resolve(node_modules, 'react-native-web'),
+    'react-native-web': path.resolve(node_modules, 'react-native-web'),
+    '@react-native-community/picker': path.resolve('../js'),
   });
 
   return config;
