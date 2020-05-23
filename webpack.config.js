5d35dd643fbc515f505bee6a01517701f1c0d7e0
Evan Bacon <baconbrix@gmail.com>
2020-05-22 21:08:13 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
-// const node_modules = path.resolve(__dirname, '..', 'node_modules');
-
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
@@ -14,13 +12,6 @@ module.exports = async function (env, argv) {
     exclude: /node_modules/,
     use: require.resolve('babel-loader'),
   });
-
-  // Object.assign(config.resolve.alias, {
-  //   react: path.resolve(node_modules, 'react'),
-  //   'react-native': path.resolve(node_modules, 'react-native-web'),
-  //   'react-native-web': path.resolve(node_modules, 'react-native-web'),
-  //   '@react-native-community/picker': path.resolve('../js'),
-  // });
 
   return config;
 };
