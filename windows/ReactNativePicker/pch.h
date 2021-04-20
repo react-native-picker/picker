d01c53bf91d4dc0c87f504f1c2da246d547d09e9
Burton Rodman <burtonrodman@hotmail.com>
2021-04-20 17:12:25 -0400

    remove unneccessaries from pch to reduce size and fix storage quota issues in build

--- windows/ReactNativePicker/pch.h
+++ windows/ReactNativePicker/pch.h
@@ -3,13 +3,5 @@
 #define NOMINMAX
 
 #include <unknwn.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Foundation.Metadata.h>
-#include <winrt/Windows.UI.Xaml.h>
-#include <winrt/Windows.UI.Xaml.Controls.h>
-#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Windows.UI.Xaml.Input.h>
-#include <winrt/Windows.UI.Xaml.Media.h>
-#include <winrt/Windows.UI.Xaml.Navigation.h>
-#include <winrt/Microsoft.ReactNative.h>
