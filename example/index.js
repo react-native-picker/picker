8058129024ae5245218773f1666eb2eef025ac14
Jesse Katsumata <jesse.katsumata@gmail.com>
2021-06-25 22:14:17 +0900

    chore: update react-native version

--- example/index.js
+++ example/index.js
@@ -1,4 +1,9 @@
+/**
+ * @format
+ */
+
 import {AppRegistry} from 'react-native';
 import App from './src/App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent('PickerExample', () => App);
+AppRegistry.registerComponent(appName, () => App);
