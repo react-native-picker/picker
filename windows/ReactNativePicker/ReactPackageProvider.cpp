3904a107a7c0fbeda2394d30dca2a8a594670535
Nick Gerleman <ngerlem@microsoft.com>
2021-08-22 12:55:49 -0700

    feat: Update RNW library to 0.64 Community Module Template
    This updates the Picker module to match the currently provided module template. This means newer cppwinrt, ARM64 support, and a pathway for WinUI 3 support (coming in a future PR).
    
    It is breaking though, as it will only correctly work with RNW 0.64+. This is arguable a bug in the current template, and I've filed https://github.com/microsoft/react-native-windows/issues/8459 to evaluate making our default template more compatible.
    
    Reducing the matrix does make it easier to make the package WinUI 3 compatible though, so it is likely worth it here. I added an entry to the README compatibility table, to be filled when a new version is released.
    
    Validated the separately updated example app functions the same as previously.

--- windows/ReactNativePicker/ReactPackageProvider.cpp
+++ windows/ReactNativePicker/ReactPackageProvider.cpp
@@ -1,14 +1,18 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "ReactPackageProvider.h"
+#if __has_include("ReactPackageProvider.g.cpp")
 #include "ReactPackageProvider.g.cpp"
+#endif
 #include "ReactPickerViewManager.h"
 
 using namespace winrt::Microsoft::ReactNative;
 
-namespace winrt::ReactNativePicker::implementation {
+namespace winrt::ReactNativePicker::implementation
+{
 
-    void ReactPackageProvider::CreatePackage(IReactPackageBuilder const& packageBuilder) noexcept {
-        packageBuilder.AddViewManager(L"ReactPickerViewManager", []() { return winrt::make<ReactPickerViewManager>(); });
-    }
+void ReactPackageProvider::CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept
+{
+    packageBuilder.AddViewManager(L"ReactPickerViewManager", []() { return winrt::make<ReactPickerViewManager>(); });
+}
 
 } // namespace winrt::ReactNativePicker::implementation
