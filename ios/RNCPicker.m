2021689a8d4c2b601b658cc55f5fc99bee8efc48
Ömer YILMAZEL <omeryilmazel@gmail.com>
2022-01-11 18:40:18 +0300

    Update RNCPicker.m

--- ios/RNCPicker.m
+++ ios/RNCPicker.m
@@ -19,7 +19,7 @@
 {
   if ((self = [super initWithFrame:frame])) {
     _color = [UIColor blackColor];
-    _font = [UIFont systemFontOfSize:24]; // TODO: selected title default should be 23.5
+    _font = [UIFont systemFontOfSize:23]; // TODO: selected title default should be 23.5
     _selectedIndex = NSNotFound;
     _textAlign = NSTextAlignmentCenter;
     _numberOfLines = 1;
@@ -86,7 +86,7 @@ numberOfRowsInComponent:(__unused NSInteger)component
 }
 
 - (CGFloat)pickerView:(__unused UIPickerView *)pickerView rowHeightForComponent:(__unused NSInteger) component {
-  return (_font.lineHeight);
+  return (_font.lineHeight) * _numberOfLines + 4;
 }
 
 - (UIView *)pickerView:(UIPickerView *)pickerView
