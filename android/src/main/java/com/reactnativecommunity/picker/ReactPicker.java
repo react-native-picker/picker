cea9766115eb26a47e5d300f4aef5fb5f03aea5c
Santi López Bayo <santitipro@hotmail.com>
2022-10-10 03:27:25 +0200

    fix: android blur method (#447)
    Co-authored-by: Santiago Lopez Bayo <santiago.bayo@bitmex.com>

--- android/src/main/java/com/reactnativecommunity/picker/ReactPicker.java
+++ android/src/main/java/com/reactnativecommunity/picker/ReactPicker.java
@@ -206,6 +206,12 @@ public class ReactPicker extends AppCompatSpinner {
     }
   }
 
+  public void clearFocus() {
+    super.setFocusableInTouchMode(true);
+    super.setFocusable(true);
+    super.onDetachedFromWindow();
+  }
+
   public void setOnSelectListener(@Nullable OnSelectListener onSelectListener) {
     mOnSelectListener = onSelectListener;
   }
