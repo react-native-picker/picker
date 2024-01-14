8668dceefecfba29816d76079c1a255a4d9a6616
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-01-14 11:47:19 +0900

    chore: fix lint in ci (#541)

--- js/PickerWindows.windows.js
+++ js/PickerWindows.windows.js
@@ -76,7 +76,7 @@ class PickerWindows extends React.Component<
           value: child.props.value,
           label: child.props.label,
           textColor: processColor(child.props.color),
-          testID: child.props.testID
+          testID: child.props.testID,
         });
       },
     );
