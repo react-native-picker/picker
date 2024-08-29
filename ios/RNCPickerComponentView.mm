c03f84f662733bb6581d3d9a80bb705ddc588b59
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-08-29 14:20:21 +0200

    feat: add load method (#580)

--- ios/RNCPickerComponentView.mm
+++ ios/RNCPickerComponentView.mm
@@ -29,6 +29,12 @@ UIPickerViewDelegate
   return concreteComponentDescriptorProvider<RNCPickerComponentDescriptor>();
 }
 
+// Needed because of this: https://github.com/facebook/react-native/pull/37274
++ (void)load
+{
+  [super load];
+}
+
 - (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
